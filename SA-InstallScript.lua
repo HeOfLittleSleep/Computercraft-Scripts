d2e5b09b87eee6b8d0aeb52197bfbcd19fa2fca5
HeOfLittleSleep <pottim99@gmail.com>
2021-01-07 16:48:40 -0600

    updated pastebin links
    the other 3 files are now in pastebin, so I updated the install script to have the proper links

--- SA-InstallScript.lua
+++ SA-InstallScript.lua
@@ -4,10 +4,10 @@ print("Is this terminal the master or slave node?\n(master/slave)")
 local input = read()
 
 if input == master then
-	shell.run("pastebin get xyzzy startup.lua")
+	shell.run("pastebin get GjcRHtyC startup.lua")
 else if input == slave then
-	shell.run("pastebin get xyzzy startup.lua")
-	shell.run("pastebin get xyzzy SA-SlaveNode.lua")
+	shell.run("pastebin get bXpAsQL6 startup.lua")
+	shell.run("pastebin get GZYKvk0W SA-SlaveNode.lua")
 else
 	error("Bad Input. please run the program again.")
 end
