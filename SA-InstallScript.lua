b8dd04c0157b746e28cc5da5e9acfcce30b25716
HeOfLittleSleep <pottim99@gmail.com>
2021-01-07 12:03:39 -0600

    Initial commit for SA-Slave-Startup.lua
    also includesinitial code for the install script

---
+++ SA-InstallScript.lua
@@ -0,0 +1,7 @@
+term.clear()
+term.setCursorPos(1, 1)
+print("Is this terminal the master or slave node?\n(master/slave)")
+local input = read()
+
+term.clear()
+print('computer id is "'..os.getComputerID()..'"')
