df6ca08a6c792ea2564df028c1c2f22bb7d3ffec
HeOfLittleSleep <pottim99@gmail.com>
2021-01-06 19:26:36 -0600

    testing

--- startupPassword.lua
+++ startupPassword.lua
@@ -9,16 +9,16 @@ while true do
 		restone.setOutput("right", true)
 		
 		term.clear()
-		--term.setCursorPos(1, 10)
-		--print("*******************ACCESS*GRANTED******************")
+		term.setCursorPos(1, 10)
+		print("*******************ACCESS*GRANTED******************")
 		sleep(10)
 		
 		restone.setOutput("left", false)
 		restone.setOutput("right", false)
 	else
 		term.clear()
-		--term.setCursorPos(1, 10)
-		--print("*******************ACCESS*DENIED*******************")
+		term.setCursorPos(1, 10)
+		print("*******************ACCESS*DENIED*******************")
 		sleep(4)
 	end
 end
