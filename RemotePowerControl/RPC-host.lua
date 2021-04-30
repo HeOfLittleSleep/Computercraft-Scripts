114a68412c4a543e6aa559065ac4d42fc31892ab
HeOfLittleSleep <pottim99@gmail.com>
2021-04-29 23:52:12 -0500

    preliminary structure setup
    setup the initial structure for RPC-host.lua

--- RemotePowerControl/RPC-host.lua
+++ RemotePowerControl/RPC-host.lua
@@ -1,13 +1,22 @@
-rednet.open("top")
+rednet.open("top")  --replace top with whatever side the modem is on
 
 while true do
 	id, msg = rednet.receive()
 
-	if msg == "jesus time" then
+	if msg == "0" then  -- set power to off
+		redstone.setOutput("left", false)
+	end
+	if msg == "1" then  --set power to on
+		redstone.setOutput("left", false)
+	end
+	if msg == "toggle" then  -- toggle power status
 		if redstone.getOutput("left") == false then
 			redstone.setOutput("left", true)
 		else
 			redstone.setOutput("left", false)
 		end
+	end
+	if msg == "q" then  -- query power status
+		redstone.getOutput("left")
 	end
 end
