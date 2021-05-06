251062d38a50fc1f453d32d84a31976d40cca5d9
HeOfLittleSleep <pottim99@gmail.com>
2021-05-06 15:22:14 -0500

    more comments, output statements added
    I finished writing documentative comments in both files. I also added some print statements for debugging as well as to provide a history of every remote connection

--- RemotePowerControl/RPC-client.lua
+++ RemotePowerControl/RPC-client.lua
@@ -2,10 +2,11 @@
 --this will send the chosen message (which needs to be one of 4 options, look in 
 --the host script code) to the specified computer hostname. running the program with
 --just the hostname parameter will default the message to "toggle"
+
 local tArgs = { ... }
 
-hostname = tArgs[1]
-if tArgs[2] == nil then
+hostname = tArgs[1]   --sets hostname to first parameter
+if tArgs[2] == nil then  --sets message to second parameter, or "toggle" if none is given
 	messageToSend = "toggle"
 else
 	messageToSend = tArgs[2]
@@ -16,8 +17,8 @@ rednet.host("RPC", "PC-Hostname")  --be sure to change "PC-Hostname" to whatever
                                    --this computer's hostname to be
 rednet.send(rednet.lookup(hostname), messageToSend, "RPC")
 
-id, rMessage,  = rednet.receive("RPC", 5)
-if rMessage == nil then
+id, rMessage, = rednet.receive("RPC", 5) --waits for confirmation from host PC. returns
+if rMessage == nil then					 --an error message if no message is received after 5 seconds
 	print("never got response from remote host")
 else
 	print(rMessage)
--- RemotePowerControl/RPC-host.lua
+++ RemotePowerControl/RPC-host.lua
@@ -1,27 +1,40 @@
+--this script will contimually wait for messages from a client computer and will
+--change the redstone output accordingly. works best if you name the file startup.lua.
+
 rednet.open("top")  --replace top with whatever side the modem is on
 rednet.host("RPC", "PC-Hostname")  --be sure to change "PC-Hostname" to whatever you want
                                    --this computer's hostname to be
 
 while true do
-	id, msg = rednet.receive()
+	print("waiting for input from clients...")
+	id, msg = rednet.receive()  --waits for message via rednet
 
+	--this block checks the recieved message and sets redstone output accordingly.
+	--It also sneds a confirmation back to the sender and prints the completed
+	--operation and the id of the computer that sent the command
 	if msg == "0" then  -- set power to off
 		redstone.setOutput("top", false)
-		rednet.send(id, "redstone output was turned off")
+		rednet.send(id, "redstone output has been turned off")
+		print("redstone output has been turned off by client: "..id)
 	elseif msg == "1" then  --set power to on
 		redstone.setOutput("top", true)
-		rednet.send(id, "redstone output was turned on")
+		rednet.send(id, "redstone output has been turned on")
+		print("redstone output has been turned on by client: "..id)
 	elseif msg == "toggle" then  -- toggle power status
 		if redstone.getOutput("top") == false then
 			redstone.setOutput("top", true)
-		rednet.send(id, "redstone output was turned on")
+			rednet.send(id, "redstone output has been turned on")
+			print("redstone output has been turned on by client: "..id)
 		else
 			redstone.setOutput("top", false)
-		rednet.send(id, "redstone output was turned off")
+			rednet.send(id, "redstone output has been turned off")
+			print("redstone output has been turned off by client: "..id)
 		end
 	elseif msg == "q" then  -- query power status
 		rednet.send(id, "redstone status is: "..redstone.getOutput())
+		print("status queried by client: "..id)
 	else
 		rednet.send(id, "invalid input received")
+		print("invalid input received from client: "..id)
 	end
 end
