ca014fdcb40340e57e9197a1c1558ede1a7cab37
HeOfLittleSleep <pottim99@gmail.com>
2021-10-28 19:29:38 -0500

    removed local prefix
    apparently then you use the local prefix on a variable within a while loop it resets after the loop is exited. removed those prefixes and cleaned up debug output

--- smelteryOverhaul.lua
+++ smelteryOverhaul.lua
@@ -1,6 +1,5 @@
 validNodeNum = false
-sNodes = "DEFAULT"
-sNodesT = "DEFAULT"
+
 
 while validNodeNum == false do
 	term.clear()
@@ -10,7 +9,7 @@ while validNodeNum == false do
 	print("[   ]")
 
 	term.setCursorPos(25, 10)
-	local sNodes = tonumber(read())
+	sNodes = tonumber(read())
 	if tonumber(sNodes) == nil or tonumber(sNodes) < 0 then
 		term.clear()
 		term.setCursorPos(16,10)
@@ -19,24 +18,9 @@ while validNodeNum == false do
 		print("Try again in 5 seconds")
 		sleep(5)
 	else
-		print("sNodes "..sNodes)
-		sleep(3)
-
-		sNodes = sNodes +1
-		print("added 1 to sNodes")
-		sleep(2)
 		validNodeNum = true
 	end	
-	term.clear()
-	print(sNodes)
-	sleep(3)
-	sNodesT = sNodes
 end
-
-term.clear()
-print(sNodes)
---print(sNodesT)
-sleep(3)
 
 while true do
 	term.clear()
@@ -45,11 +29,11 @@ while true do
 	term.setCursorPos(24, 10)
 	print("[   ]")
 	term.setCursorPos(25, 10)
-	local QueueTotal = tonumber(read())
+	QueueTotal = tonumber(read())
 	
 	if tonumber(QueueTotal) == nil or tonumber(QueueTotal) < 1 then
 		term.clear()
-		term.setCursorPos(6,10)
+		term.setCursorPos(16,10)
 		print("Invalid input fuckwit!")
 		term.setCursorPos(14,11)
 		print("Try again in 5 seconds")
@@ -61,33 +45,26 @@ while true do
 		term.setCursorPos(24, 10)
 		print("[   ]")
 		term.setCursorPos(25, 10)
-		local WaitTimer = tonumber(read())
+		WaitTimer = tonumber(read())
 		
 		if tonumber(WaitTimer) == nil or tonumber(WaitTimer) < 1 then
 			term.clear()
-			term.setCursorPos(6,10)
+			term.setCursorPos(16,10)
 			print("Invalid input fuckwit!")
 			term.setCursorPos(14,11)
 			print("Try again in 5 seconds")
 			sleep(5)
 		else
-			print("QueueTotal"..QueueTotal.." | ")
-			print(sNodesT)
-			sleep(3)
-			local sQueue = math.floor(QueueTotal / (sNodesT + 1))
-			local mQueue = QueueTotal - (sQueue * (sNodesT + 1)) + sQueue
+			sQueue = math.floor(QueueTotal / (sNodes + 1))
+			mQueue = QueueTotal - (sQueue * (sNodes + 1)) + sQueue
 
-			--if sNodes > 0 then
-				--rednet.open("top")
-				--rednet.broadcast("SA-SlaveNode "..sQueue.." "..WaitTimer)
-				--rednet.close("top")
-			--end
-
-			print("mQueue is "..mQueue)
-			sleep(3)
+			if sNodes > 0 then
+				rednet.open("top")
+				eednet.broadcast("SA-SlaveNode "..sQueue.." "..WaitTimer)
+				rednet.close("top")
+			end
 
 			for i = 1, mQueue, 1 do
-				print("got into for loop")
 				sleep(3)
 				term.clear()
 				term.setCursorPos(1, 1)
