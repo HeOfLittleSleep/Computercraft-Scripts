9256f42d814cf749ec6f1294643511ff56b18788
HeOfLittleSleep <pottim99@gmail.com>
2023-07-18 13:44:43 -0500

    added basic error handling
    after coming back to this program after a long while, I realized that my error handling was shit and non-existant. the script will now tell you if you failed to enter a hostname parameter, or if you fucked up the hostname and it doesn't show up in rednet

--- RemotePowerControl/RPC-client.lua
+++ RemotePowerControl/RPC-client.lua
@@ -3,10 +3,15 @@
 --the host script code) to the specified computer hostname. running the program with
 --just the hostname parameter will default the message to "toggle"
 
-local tArgs = { ... }
+local tArgs = { ... } --collect parameters from run command
 
-hostname = tArgs[1]   --sets hostname to first parameter
-if tArgs[2] == nil then  --sets message to second parameter, or "toggle" if none is given
+if tArgs[1] == nil then  --sets hostname to first parameter if first parameter is not nil
+	error('Error:  missing hostname parameter.\nPlease run again as "script.lua [remote hostname]"')
+else
+	hostname = tArgs[1]
+end
+
+if tArgs[2] == nil then  --sets message to second parameter, or to "toggle" if nil
 	messageToSend = "toggle"
 else
 	messageToSend = tArgs[2]
@@ -15,10 +20,13 @@ end
 rednet.open("back")
 rednet.host("RPC", "client-PC")  --be sure to change "client-PC" to whatever you want
                                  --this computer's hostname to be
+if rednet.lookup("RPC", hostname) == nil then  --checks that hostname is valid
+	error("Error:  host not found"
+end
 rednet.send(rednet.lookup("RPC", hostname), messageToSend, "RPC")
 
 id, rMessage, protocol = rednet.receive("RPC") --waits for confirmation from host PC. returns
-if rMessage == nil then					--an error message if no message is received after 5 seconds
+if rMessage == nil then					       --an error message if no message is received after 5 seconds
 	print("never got response from remote host")
 	rednet.close("back")
 else
