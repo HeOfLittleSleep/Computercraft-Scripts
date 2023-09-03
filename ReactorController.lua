b9d8b9aac17ebe1167540aded18e4480df5afa53
HeOfLittleSleep <pottim99@gmail.com>
2023-09-02 23:51:19 -0500

    pulled initialization code from test scriptfrom
    basically just copied the rest of the initialization I have in the testing script

--- ReactorController.lua
+++ ReactorController.lua
@@ -1,5 +1,9 @@
 --program that will monitor and regulate a big reactor
 
+redSide = nil
+reactors = {peripheral.find("BigReactors-Reactor")}
+reactor = reactors[1]
+
 function isRedSideValid()
 	validSides = {"left", "right", "front", "back"}
 
@@ -15,4 +19,69 @@ function isRedSideValid()
 	end
 
 	return valueFound
+end
+
+term.clear()
+if
+	fs.exists("rc-RedIn") == false  --checks if file exists
+then
+	term.setCursorPos(1, 1)
+	print("Define which side to monitor for redstone power.")
+	print("  Options are: front, back, left, or right")
+	term.setCursorPos(13, 9)
+	print("------------------------")
+	term.setCursorPos(13, 10)
+	print("|                      |")
+	term.setCursorPos(13, 11)
+	print("------------------------")
+
+	term.setCursorPos(14, 10)
+	input = read()
+
+	if input == "left" or
+	input == "right" or
+	input == "front" or
+	input == "back" then
+		fs.open("rc-RedIn", "w")
+		local fh = fs.open("rc-RedIn", "a")
+		h.flush()
+		h.writeLine(input)
+		h.close()
+
+		redSide = input
+	else
+		error("Invalid input submited. Run program again")
+	end
+
+
+
+	term.clear()
+	term.setCursorPos(1, 1)
+else
+	local fh = fs.open("rc-RedIn", "r")
+	redSide = fh.readLine()
+	print(redSide)
+	h.close()
+end
+
+if redSide ~= nil then
+	--redstone.setOutput(redSide, true)
+	print("redstone monitor side was set to "..redSide)
+else
+	error("data read from config was NIL")
+end
+
+if
+	reactor ~= nil
+then
+	if
+		reactor.mbIsAssembled()
+	then
+		print("reactor was found!")
+		print(redstone.getAnalogInput(redSide))
+	else
+		print("Reactor was found, but is not functional")
+	end
+else
+	print("Reactor was not found")
 end
