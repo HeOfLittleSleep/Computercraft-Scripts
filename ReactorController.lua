851c0ee62cb79d215dcd76e923a527ddea316155
HeOfLittleSleep <pottim99@gmail.com>
2023-09-02 23:22:04 -0500

    added isRedSideValid() funtion
    added a function that will check configuration provided by the user and validate it

--- ReactorController.lua
+++ ReactorController.lua
@@ -1,1 +1,18 @@
 --program that will monitor and regulate a big reactor
+
+function isRedSideValid()
+	validSides = {"left", "right", "front", "back"}
+
+	local fh = fs.open("rc-RedIn", "r")
+	redSide = fh.readLine()
+	h.close()
+
+	valueFound = false
+	for i = 1, #list, 1 do
+		if list[i] == redSide then
+			valueFound = true
+		end
+	end
+
+	return valueFound
+end
