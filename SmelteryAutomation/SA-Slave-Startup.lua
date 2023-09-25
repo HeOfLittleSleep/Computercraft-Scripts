04912a1100644f2627395899984df801ccd88350
HeOfLittleSleep <pottim99@gmail.com>
2023-09-25 14:35:56 -0500

    Slave node scripts have been combined into 1
    SA-Slave-Startup.lua now knows what to do with the info the master node sends, and eliminates the need to call a secondary script

--- SmelteryAutomation/SA-Slave-Startup.lua
+++ SmelteryAutomation/SA-Slave-Startup.lua
@@ -3,17 +3,22 @@ term.setCursorPos(1, 1)
 rednet.open("top")
 rednet.host("smelteryAuto", tostring(os.getComputerID()))
 
---id, msg = rednet.receive("smelteryAuto")
-
 while true do
-	--term.clear()
 	term.setCursorPos(1, 1)
 	print("Waiting for orders from Master node...")
 	id, msg = rednet.receive("smelteryAuto")
 	queue = msg[1]
 	timer = msg[2]
 
-	print(queue)
-	print(timer)
-	--shell.run(msg)
+	for i = 1, queue, 1 do
+		term.clear()
+		term.setCursorPos(1, 1)
+		print("un-moltened "..(i - 1).." out of "..queue.." things")
+
+		redstone.setOutput("bottom", true)
+		sleep()
+		redstone.setOutput("bottom", false)
+
+		sleep(timer)
+	end
 end
